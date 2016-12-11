495c141526323aaf3f2547362fba7f0c54129542
Mrucznik <mrucznix@gmail.com>
2016-12-11 01:48:15 +0100

    Klasy

---
+++ LostAndFound/Classes/Announcement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lostandfoundapp.Classes
+{
+    public class Announcement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public User Owner { get; set; }
+        public DateTime Date { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
